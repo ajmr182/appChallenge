f45411534af1d7fe0a29e787f31604a0fe767e3e
Amilcar Malaver <ajmr182@gmail.com>
2021-11-27 22:04:36 +0100

    Segunda Version App

--- app/src/main/java/com/example/app/fragments/descripcionFragment/DescripcionFragment.kt
+++ app/src/main/java/com/example/app/fragments/descripcionFragment/DescripcionFragment.kt
@@ -1,12 +1,12 @@
 package com.example.app.fragments.descripcionFragment
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.example.app.R
 import com.example.app.databinding.FragmentDescripcionBinding
 import com.example.app.viewModel.AgendaViewModel
 
@@ -20,16 +20,22 @@ class DescripcionFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
+        setHasOptionsMenu(true)
         _binding = FragmentDescripcionBinding.inflate(inflater, container, false)
         mAgendaViewModel = ViewModelProvider(this).get(AgendaViewModel::class.java)
+        binding.toolbar2.setTitle(args.currentItem.nombreTarea).toString()
+        binding.toolbar2.inflateMenu(R.menu.descripcion_menu)
         binding.tvFinal.setText(args.currentItem.finalizaTarea).toString()
         binding.tvDescripcionTarea.setText(args.currentItem.contenidoTarea).toString()
 
         return binding.root
     }
 
+    /*override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.descripcion_menu, menu)
+    }
+*/
     override fun onDestroyView() {
 
         super.onDestroyView()
